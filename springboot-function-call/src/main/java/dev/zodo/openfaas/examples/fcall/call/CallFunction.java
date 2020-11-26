334f2a461477a2d4322dff9c10e9a9f59e9fe2bd
Edimar Cardoso <edimar.cardoso@tecsinapse.com.br>
2020-11-26 00:39:30 -0300

    Fix lib implementation

--- springboot-function-call/src/main/java/dev/zodo/openfaas/examples/fcall/call/CallFunction.java
+++ springboot-function-call/src/main/java/dev/zodo/openfaas/examples/fcall/call/CallFunction.java
@@ -9,23 +9,34 @@ import dev.zodo.openfaas.examples.fcall.vo.CalculatorData;
 import dev.zodo.openfaas.examples.fcall.vo.Operator;
 import dev.zodo.openfaas.examples.fcall.vo.ResultData;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+import java.net.URI;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Slf4j
+@Service
 public class CallFunction {
-    public static void main(String[] args) {
-        callSyncCalculator(10d, 20d);
-        asyncCallSyncCalculator(10d, 20d);
+
+    @Value("${openfaas.provider.url}")
+    private String url;
+    @Value("${openfaas.provider.username}")
+    private String username;
+    @Value("${openfaas.provider.password}")
+    private String password;
+
+    private OpenfaasApi openfaasApi() {
+        return OpenfaasApi.getInstance(URI.create(url), username, password);
     }
 
-    public static void callSyncCalculator(double value1, double value2) {
+    public void callSyncCalculator(double value1, double value2) {
         log.info("[callSyncCalculator] {}", LocalDateTime.now());
         CalculatorData data = new CalculatorData(Operator.SUM, value1, value2);
         SyncRequest<CalculatorData> request = new SyncRequest<>("calculator", data);
-        SyncResponse<ResultData> response = OpenfaasApi.getInstance().callFunction(request, ResultData.class);
+        SyncResponse<ResultData> response = openfaasApi().callFunction(request, ResultData.class);
         ResultData resultData = response.getBody();
         log.info("Duration {}ms", Optional.ofNullable(response.getDurationSeconds())
                 .map(Duration::toMillis)
@@ -33,11 +44,11 @@ public class CallFunction {
         log.info("{} {} {} = {}", resultData.getValue1(), resultData.getOperator().getSymbol(), resultData.getValue2(), resultData.getResult());
     }
 
-    public static void asyncCallSyncCalculator(double value1, double value2) {
+    public void asyncCallSyncCalculator(double value1, double value2) {
         log.info("[asyncCallSyncCalculator] {}", LocalDateTime.now());
         CalculatorData data = new CalculatorData(Operator.SUM, value1, value2);
         AsyncRequest<CalculatorData> request = new AsyncRequest<>("calculator", "http://10.0.0.31:8080/api/openfaas/async-callback", data);
-        AsyncResponse response = OpenfaasApi.getInstance().callAsyncFunction(request);
+        AsyncResponse response = openfaasApi().callAsyncFunction(request);
         log.info("CallId {}", response.getCallId());
         log.info("StartTime {}", response.getStartTime());
         log.info("StatusCode {}", response.getStatusCode());
--- springboot-function-call/src/main/java/dev/zodo/openfaas/examples/fcall/call/CallScheduled.java
+++ springboot-function-call/src/main/java/dev/zodo/openfaas/examples/fcall/call/CallScheduled.java
@@ -1,22 +1,26 @@
 package dev.zodo.openfaas.examples.fcall.call;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalTime;
 
 @Component
+@RequiredArgsConstructor
 public class CallScheduled {
+
+    private final CallFunction callFunction;
 
     @Scheduled(fixedDelay = 15_000)
     public void callSync() {
         LocalTime time = LocalTime.now();
-        CallFunction.callSyncCalculator(time.getMinute(), time.getSecond());
+        callFunction.callSyncCalculator(time.getMinute(), time.getSecond());
     }
 
     @Scheduled(fixedDelay = 16_000, initialDelay = 10_000)
     public void callAsync() {
         LocalTime time = LocalTime.now();
-        CallFunction.asyncCallSyncCalculator(time.getMinute(), time.getSecond());
+        callFunction.asyncCallSyncCalculator(time.getMinute(), time.getSecond());
     }
 }
